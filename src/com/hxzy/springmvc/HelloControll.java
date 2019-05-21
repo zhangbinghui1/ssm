c725e54641dc2ac5ba792216d751e08f701e0f04
Administrator <Administrator@DEEP-1904101044>
2019-05-21 18:42:49 +0800

    第一次提交git 本地

---
+++ src/com/hxzy/springmvc/HelloControll.java
@@ -0,0 +1,20 @@
+package com.hxzy.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloControll {
+	
+	@RequestMapping("Hello")
+	public ModelAndView hello() {
+		ModelAndView mv=new ModelAndView();
+		//设置模型数据
+		mv.addObject("msg","hello springmvc");
+		//设置视图
+		mv.setViewName("hello");
+		return mv;
+	}
+
+}
