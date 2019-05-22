09dfb15838164e01663509cf61e5ea1c036f6611
Administrator <Administrator@DEEP-1904101044>
2019-05-22 08:39:52 +0800

    333333

--- src/com/hxzy/springmvc/ItemControll.java
+++ src/com/hxzy/springmvc/ItemControll.java
@@ -15,10 +15,12 @@ public class ItemControll {
 		@RequestMapping("list")
 		public ModelAndView getList() {
 			ModelAndView mv=	new ModelAndView();
-			List<Item> items=Arrays.asList(new Item(1,"冰箱",1999,new Date(),"冰箱很好"),new Item(1,"洗衣机",1999,new Date(),"冰箱很好"));
+			List<Item> items=Arrays.asList(new Item(1,"鍐扮",1999,new Date(),"鍐扮寰堝ソ"),new Item(1,"娲楄。鏈�",1999,new Date(),"鍐扮寰堝ソ"));
 			mv.addObject("itemlist",items);
 			mv.setViewName("item");
-			System.out.println("mikee");
+			System.out.println("mikeee");
+			System.out.println("mim88888");
+			
 			
 			return mv;
 		}
